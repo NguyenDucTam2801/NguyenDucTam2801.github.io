9dbc83feb6ab8879940c927df1dde78e875a0672
Tamu <nguyenductam12003@gmail.com>
2026-05-30 21:35:34 +0700

    Fix hero size image on about me page

--- src/pages/AboutMePage.tsx
+++ src/pages/AboutMePage.tsx
@@ -1,7 +1,6 @@
 import { SocialIconsHero } from "../data/homePage/SocialLinkData";
 import HeroSection from "../sections/aboutMePage/HeroSection";
 import { heroSectionData, resumeLink } from "../data/aboutPage/heroSectionData";
-import { FlexDiv } from "../components/ui/FlexDiv";
 import aboutMeHero from '../assets/images/aboutMeHero.jpg'
 import MyCapabilitiesSection from "../sections/aboutMePage/MyCapabilitiesSection";
 import { myCapabilitiesSectionData, listSkills } from "../data/aboutPage/myCapabilitiesSectionData";
@@ -11,25 +10,30 @@ import MyAcademySection from "../sections/aboutMePage/MyAcademySection";
 import { academyList, myAcademySectionData } from "../data/aboutPage/myAcademySectionData";
 import { MyLocationSection } from "../sections/aboutMePage/MyLocation";
 import { googleMapsEmbed, googleMapsLink, myLocationSectionData, } from "../data/aboutPage/myLocationSectionData";
+import { FlexSection } from "../components/ui/FlexSection";
 
 const AboutMePage = () => {
     return (
         <main className="overflow-x-hidden">
             <HeroSection heroSectionData={heroSectionData} socialIconsData={SocialIconsHero} resumeLink={resumeLink} />
-            <FlexDiv
+            <FlexSection
                 direction="col"
                 align="center"
                 justify="center"
                 // Added 'overflow-visible' and 'relative' to the container
-                className="w-[90%] md:w-[80%] mx-auto bg-none md:bg-gray-500 rounded-md overflow-visible relative"
+                className="bg-none md:bg-gray-500 rounded-md overflow-visible relative"
                 margin="my-20"
             >
                 <img
                     src={aboutMeHero}
                     alt="About Me"
-                    className="w-full md:w-[40%] h-auto hover:scale-105 transition-transform duration-300 relative hover:z-10 rounded-3xl md:rounded-none"
+                    // 1. Removed width: '100%' from style (kept objectFit)
+                    style={{ objectFit: 'cover' }}
+                    // 2. Added w-[90%] md:w-[40%]
+                    // 3. Changed transition-all to transition-transform for better animation performance
+                    className=" md:w-[40%] rounded-3xl md:rounded-none hover:scale-105 transition-transform duration-300 md:m-0 my-5"
                 />
-            </FlexDiv>
+            </FlexSection>
             <MyCapabilitiesSection myCapabilitiesData={myCapabilitiesSectionData} skillList={listSkills} />
             <hr className='border-gray-600 w-full' />
             <MyExperienceSection experienceSectionData={myExperienceSectionData} experienceItems={experienceList} />
